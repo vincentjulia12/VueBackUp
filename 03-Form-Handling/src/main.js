e14560f440c7b609e8a8191523b77219018f9c75
unknown <vincentjulia25@gmail.com>
2022-12-02 20:41:45 +0800

    lesson 3

---
+++ 03-Form-Handling/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+// Font Awesome CSS
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
+
+//Bootstrap CSS, JS
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+
+createApp(App).use(store).use(router).mount('#app')
