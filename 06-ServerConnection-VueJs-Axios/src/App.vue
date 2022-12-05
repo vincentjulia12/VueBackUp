6d421229f335673b07f88e878d6bc2a7857e777a
unknown <vincentjulia25@gmail.com>
2022-12-05 21:14:05 +0800

    done lesson 6

---
+++ 06-ServerConnection-VueJs-Axios/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="grid">
+      <div class="row">
+        <div class="col">
+         
+        </div>
+      </div>
+    </div>
+
+    <UserListComponent/>
+  </div>
+  
+</template>
+
+<script>
+import UserListComponent from "./components/UserListComponent.vue"
+
+export default {
+  name: 'App',
+  components: {
+    UserListComponent
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./style/main.scss";
+
+</style>
---
+++ 06-ServerConnection-VueJs-Axios/src/components/SpinnerComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <img src="../assets/loading.gif" class="d-block m-auto" alt="" >
+  </div>
+</template>
+
+<script>
+export default {
+    name: "SpinnerComponent"
+};
+</script>
+
+<style>
+
+</style>
