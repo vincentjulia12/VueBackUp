777126d241a8c5e31811b047ce392f1d6da5ec32
unknown <vincentjulia25@gmail.com>
2022-12-05 13:53:42 +0800

    adding new file

--- 03-Form-Handling/src/components/RegisterComponent.vue
+++ 03-Form-Handling/src/components/RegisterComponent.vue
@@ -2,14 +2,23 @@
   <div class="container">
     <div class="row">
       <div class="col-md-3">
-        <div class="card">
+        <div class="card shadow-lg">
           <div class="card-header bg-success text-white">
-            <p class="h3">Register Here</p>
+            <p id="register-header" class="h3">Register Here</p>
           </div>
           <div class="card-body bg-light">
-            <form>
+            <form @submit.prevent="register()">
               <div class="mb-2">
-                <input type="text" class="form-control" placeholder="Name:">
+                <input v-model="user.name" type="text" class="form-control" placeholder="Name:">
+              </div>
+              <div class="mb-2">
+                <input v-model="user.email" type="email" class="form-control" placeholder="Email:">
+              </div>
+              <div class="mb-2">
+                <input v-model="user.password" type="password" class="form-control" placeholder="Password:">
+              </div>
+              <div class="mb-2 mt-3">
+                <input type="submit" class="btn btn-danger" value="Register">
               </div>
             </form>
           </div>
@@ -21,10 +30,25 @@
 
 <script>
 export default {
-    name: "RegisterComponent"
-}
+  name: "RegisterComponent",
+  data() {
+    return {
+      user: {
+        name: "",
+        email: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    register() {
+      console.log(this.user);
+    }
+  }
+};
 </script>
 
-<style>
+<style lang="scss">
+@import "./src/style/main.scss";
 
 </style>
