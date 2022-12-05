afb2f5cb25549469ccb502315984b2112d8d062c
unknown <vincentjulia25@gmail.com>
2022-12-05 18:46:19 +0800

    list rendering done

---
+++ 05-List-Rendering/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="grid">
+      <div class="row">
+        <div class="col">
+          <p id="p1" class="h3 mt-3">App Component</p>
+          <p id="p2" class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis qui deserunt error. Culpa cum odio beatae libero accusamus, itaque facere minima consectetur voluptatibus optio molestias nemo porro asperiores magnam natus.</p>
+        </div>
+      </div>
+    </div>
+
+   
+    <UserListComponent/>
+  </div>
+  
+</template>
+
+<script>
+import UserListComponent from "./components/UserListComponent.vue"
+export default {
+  name: 'App',
+  components: {
+    UserListComponent
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./style/main.scss";
+
+</style>
---
+++ 05-List-Rendering/src/components/UserListComponent.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <p id="p1" class="h3">User List</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quam repudiandae animi aut eveniet quisquam, exercitationem maxime quasi nulla praesentium perferendis in velit perspiciatis sunt tempora ducimus pariatur dolores magnam.</p>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <table class="table table-hover text-center ">
+                        <thead class="bg-dark text-white">
+                            <tr>
+                                <th>no.</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Compony</th>
+                                <th>Website</th>
+                                <th>Location</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="user of users" :key="user.id">
+                                <td>{{user.id}}</td>
+                                <td>{{user.name}}</td>
+                                <td>{{user.email}}</td>
+                                <td>{{user.company.name}}</td>
+                                <td>{{user.website}}</td>
+                                <td>{{user.address.city}}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { UserService } from '@/services/UserService';
+
+export default {
+    name: "UserListComponent",
+    data() {
+        return {
+            users: UserService.getAllUsers()
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
