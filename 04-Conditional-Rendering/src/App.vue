3f0d547c245ff52cfc335d72b2928e5ad08d7d31
unknown <vincentjulia25@gmail.com>
2022-12-05 16:17:11 +0800

    conditional rendering

---
+++ 04-Conditional-Rendering/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="grid">
+      <div class="row">
+        <div class="col">
+          <p id="p1" class="h3 mt-3">App Component</p>
+          <p id="p2" class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis qui deserunt error. Culpa cum odio beatae libero accusamus, itaque facere minima consectetur voluptatibus optio molestias nemo porro asperiores magnam natus.</p>
+        </div>
+      </div>
+    </div>
+
+    <AuthUserComponent/>
+
+  </div>
+  
+</template>
+
+<script>
+import AuthUserComponent from "./components/AuthUserComponent.vue"
+export default {
+  name: 'App',
+  components: {
+    AuthUserComponent
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./style/main.scss";
+
+</style>
---
+++ 04-Conditional-Rendering/src/components/AuthUserComponent.vue
@@ -0,0 +1,53 @@
+<template>
+    <!-- <pre>{{isLoggedIn}}</pre> -->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <div id="div-1" v-if="isLoggedIn">
+                            <p id="p1" class="h3">Welcome User</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem debitis qui deleniti, molestiae voluptatibus quis. Est explicabo consequuntur culpa quidem repellendus animi, provident optio nisi nobis libero enim vitae ipsa.</p>
+                        </div>
+                        <div id="div-2" v-if="!isLoggedIn">
+                            <p id="" class="h3 text-danger">Thank you</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem debitis qui deleniti, molestiae voluptatibus quis. Est explicabo consequuntur culpa quidem repellendus animi, provident optio nisi nobis libero enim vitae ipsa.</p>
+                        </div>
+                        <div id="main-container">
+                            <div id="left-section">
+                                <button v-if="!isLoggedIn" class="btn btn-success btn-sm m-1" @click="login()">Login</button>
+                            </div>
+                            <div id="right-section">
+                                <button v-if="isLoggedIn" class="btn btn-danger btn-sm m-1 " @click="logout()">Logout</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "AuthUserComponent",
+    data(){
+        return {
+            isLoggedIn: false 
+        };
+    },
+    methods: {
+        login() {
+            this.isLoggedIn = true;
+        },
+        logout() {
+            this.isLoggedIn = false;
+        }
+    }
+};
+</script>
+
+<style lang="scss">
+@import "../style/main.scss"
+</style>
